bbc4477e148d37a1a838da60efe3e416475b6fcb
Brendon Zhang <z.brendon97@gmail.com>
2018-01-22 19:14:10 -0500

    Create moveTitle.js
    js file that takes the cursor x and y coordinates on the webpage and shifts a 'title2' id

---
+++ moveTitle.js
@@ -0,0 +1,23 @@
+function shift(event){
+				var x = event.clientX;
+				var y = event.clientY;
+				var xmiddle = window.innerWidth/2;
+				var ymiddle = window.innerHeight/2;
+				var temp;
+				if(x > xmiddle){
+					temp = 6*(1-(x/xmiddle));
+					document.getElementById('title2').style.marginLeft = temp + "px";
+					
+				}else{
+					temp = -6*((x/xmiddle)-1);
+					document.getElementById('title2').style.marginLeft = temp + "px";
+				}
+
+				if(y > ymiddle){
+					temp = 8*(1-(y/ymiddle));
+					document.getElementById('title2').style.marginTop = temp + "px";
+				}else{
+					temp = -8*((y/ymiddle)-1);
+					document.getElementById('title2').style.marginTop = temp + "px";
+				}
+			}
